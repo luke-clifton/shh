b2c521f1811e01f69aa36462d05739baa6c1b834
Luke Clifton <lukec@themk.net>
2019-03-07 20:43:13 +0800

    Fix bounds

--- shell.nix
+++ shell.nix
@@ -7,7 +7,7 @@ let
   f = { mkDerivation, async, base, coreutils, deepseq, directory
       , filepath, hashable, mtl, perl, process, split, stdenv, tasty
       , tasty-hunit, tasty-quickcheck, template-haskell, temporary, unix
-      , vim, hostname
+      , vim, hostname, Glob
       }:
       mkDerivation {
         pname = "shh";
@@ -17,7 +17,7 @@ let
         isExecutable = true;
         libraryHaskellDepends = [
           async base deepseq directory filepath mtl process split
-          template-haskell unix hostname temporary
+          template-haskell unix hostname temporary Glob
         ];
         executableHaskellDepends = [
           async base directory hashable split temporary
--- shh-extras/src/Shh/Prompt.hs
+++ shh-extras/src/Shh/Prompt.hs
@@ -1,11 +1,31 @@
+-- | This module provides utility functions for creating prompts. Useful
+-- if you want to use Shh and GHCi as a shell.
 module Shh.Prompt where
 
 import Shh
 import System.Environment
 import Network.HostName
+import Data.Time
 
+-- | The type of GHCi prompt functions
 type PromptFn = [String] -> Int -> IO String
 
+-- | Format a prompt line suitable for use with Shh.
+--
+-- This also calls `initInteractive`, which is required for a good
+-- user experience when using `shh` as a shell.
+--
+-- The format of the prompt uses the "%" character as an escape mechanism,
+-- allowing for the substitution of various values into the prompt.
+--
+-- * @%% -> %@
+-- * @%u -> current user@
+-- * @%w -> current directory@
+-- * @%h -> hostname@
+-- * @%t -> HH:MM:SS@
+--
+-- Use it by importing @Shh.Prompt@ in your @.ghci@ or @$SHH_DIR/init.ghci@
+-- files and @:set prompt-function formatPrompt "%u\@%h:%w$ "@
 formatPrompt :: String -> PromptFn
 formatPrompt fmt _ _ = do
     initInteractive
@@ -17,6 +37,7 @@ formatPrompt fmt _ _ = do
             format ('%' : 'u' : rest) = insertEnv "USER" rest
             format ('%' : 'w' : rest) = insertEnv "PWD" rest
             format ('%' : 'h' : rest) = insertIO getHostName rest
+            format ('%' : 't' : rest) = insertIO getTime rest
             format ( x  : rest) = (x:) <$> format rest
             format [] = pure []
 
@@ -25,3 +46,8 @@ formatPrompt fmt _ _ = do
 
             insertIO :: IO String -> String -> IO String
             insertIO a rest = a >>= \s -> (s ++) <$> format rest
+
+            getTime :: IO String
+            getTime = formatTime defaultTimeLocale "%H:%M:%S" <$>
+                (utcToLocalTime <$> getCurrentTimeZone <*> getCurrentTime)
+
