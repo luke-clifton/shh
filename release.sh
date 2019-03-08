109b966b829b9deff7a92c29414e0db8d62187a9
Luke Clifton <lukec@themk.net>
2019-03-08 18:03:35 +0800

    Add release script

---
+++ release.sh
@@ -0,0 +1,72 @@
+#! /usr/bin/env bash
+
+set -e
+
+hackageVersion() {
+    curl -fsS -H 'Accept: application/json' "https://hackage.haskell.org/package/$1/preferred" \
+        | jq -r '."normal-version"[0]'
+}
+
+localVersion() {
+    cat "$1/$1.cabal" | grep '^version:' | tr -d '[:space:]' | cut -d: -f2
+}
+
+needsUpload() {
+    if [ "$(hackageVersion "$1")" = "$(localVersion "$1")" ]
+    then
+        return 1
+    else
+        return 0
+    fi
+}
+
+upload() {
+    if needsUpload "$1"
+    then
+        if sanity "$1"
+        then
+            toupload="$1-$(localVersion "$1")"
+            echo "Uploading $toupload"
+            read -p 'continue? (type "yes") ' answer
+            if ! [ "$answer" = "yes" ]
+            then
+                exit 1
+            fi
+            tmp=$(mktemp -d)
+            cabal new-sdist --verbose=0 --builddir="$tmp" "$1"
+            cabal new-haddock --haddock-for-hackage --builddir="$tmp" "$1"
+            cabal upload --publish "$tmp"/sdist/"${toupload}.tar.gz"
+            cabal upload --publish -d "$tmp"/"${toupload}"-docs.tar.gz
+            git tag -a "$toupload" -m "Releaseing $toupload"
+        else
+            echo "$1 has issues, not uploading"
+        fi
+    else
+        echo "$1 was not uploaded (versions match)"
+        if ! codeMatch "$1"
+        then
+            echo "... but there is a difference in the tarballs!"
+            echo "... bump the version number to upload!"
+        fi
+    fi
+}
+
+codeMatch() {
+    tmp=$(mktemp -d)
+    cabal new-sdist --verbose=0 --builddir="$tmp" "$1"
+    cmp -s "$tmp"/sdist/*.tar.gz \
+        <(curl -fsS "https://hackage.haskell.org/package/$1/$1-$(hackageVersion "$1").tar.gz")
+    rm -Rf "$tmp"
+}
+
+sanity() {
+    cabal new-build "$1" || return 1
+    cabal new-test "$1" || return 1
+    if cabal new-haddock --haddock-for-hackage "$1" | grep -A 5 'Missing documentation'
+    then
+        return 1
+    fi
+}
+
+upload shh
+upload shh-extras
