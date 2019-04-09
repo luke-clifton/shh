6d861a47c81a7d8da0e7c8e7ac78ee686fae838b
Luke Clifton <lukec@themk.net>
2019-04-09 11:59:47 +0800

    Run checks that shh and shh-extras are installed when running generating an initial init.ghci file.

--- shh/app/shh-app.hs
+++ shh/app/shh-app.hs
@@ -4,6 +4,7 @@ module Main where
 import Shh
 import System.IO
 import System.Environment
+import System.Exit
 import System.IO.Temp
 import System.Directory
 import Data.Hashable (hash)
@@ -19,6 +20,9 @@ defaultShell = "\
 defaultInitGhci = "\
 \:seti -XNoOverloadedLists\n\
 \import Shh\n\
+\ "
+
+extraInitGhci = "\
 \import Shh.Prompt\n\
 \:set prompt-function formatPrompt \"\\n\\ESC[1;32m[%u@%h:%w]λ \\ESC[0m\"\n\
 \:set prompt-cont \"| \"\n\
@@ -32,11 +36,14 @@ defaultWrapper = "\
 
 debug = putStrLn
 
-writeIfMissing :: FilePath -> String -> IO ()
-writeIfMissing fp s = do
+doIfMissing :: FilePath -> IO () -> IO ()
+doIfMissing fp a = do
     doesFileExist fp >>= \case
         True -> pure ()
-        False -> writeFile fp s
+        False -> a
+
+writeIfMissing :: FilePath -> String -> IO ()
+writeIfMissing fp s = doIfMissing fp (writeFile fp s)
 
 main :: IO ()
 main = do
@@ -57,13 +64,29 @@ main = do
     createDirectoryIfMissing False shhDir
 
     withCurrentDirectory shhDir $ do
-        writeIfMissing "init.ghci" defaultInitGhci
         writeIfMissing "wrapper" defaultWrapper
         setPermissions "wrapper" $
             setOwnerExecutable True $
             setOwnerReadable True $
             setOwnerWritable True $
             emptyPermissions
+        doIfMissing "init.ghci" $ do
+            catchFailure (exe wrapper "ghc-pkg" "latest" "shh") >>= \case
+                Left _ -> do
+                    putStrLn "Please make the shh and shh-extras packages available in the shh"
+                    putStrLn "environment (install it globally or modify the wrapper, see docs)."
+                    putStrLn "Aborting"
+                    exitFailure
+                Right _ -> writeFile "init.ghci" defaultInitGhci
+            catchFailure (exe wrapper "ghc-pkg" "latest" "shh-extras") >>= \case
+                Left _ -> do
+                    putStrLn "## WARNING ##########################################################"
+                    putStrLn "# You do not have the shh-extras library installed, and so we are"
+                    putStrLn "# generating a reduced functionality init.ghci file. To restore full"
+                    putStrLn "# functionality, install shh-extras and re-generate your init.ghci"
+                    putStrLn $ "# file by deleting " <> shhDir <> "/init.ghci and re-running shh"
+                    putStrLn "#####################################################################"
+                Right _ -> appendFile "init.ghci" extraInitGhci
         writeIfMissing "Shell.hs" defaultShell
 
     runProc $ mkProc' True wrapper ["ghci", "-ghci-script", shhDir <> "/init.ghci", shhDir <> "/Shell.hs"]
