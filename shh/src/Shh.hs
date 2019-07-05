64bc3a9341f6c63c627a229374bfe4de999ed6f3
Luke Clifton <lukec@themk.net>
2019-07-05 21:42:11 +0800

    Remove a ton of redundant functions

--- shh/src/Shh.hs
+++ shh/src/Shh.hs
@@ -6,7 +6,8 @@ module Shh
     -- | == Running a `Proc`
     , Shell(..)
     -- | == Constructing a `Proc`
-    , Cmd
+    -- You usually don't need to @`runProc`@ because most functions in shh
+    -- are polymorphic in their return type.
     -- | === External Processes
     -- These allow for the construction of @`Proc`@s that call external
     -- processes. You will often use the TemplateHaskell functions below
@@ -15,29 +16,38 @@ module Shh
     , mkProc
     , mkProc'
     , Proc()
-    -- | === "Native" Processes
+    -- | === "Native" Processes (Lazy)
     -- You can also create native Haskell @`Proc`@s which behave the same
     -- way, but simply run Haskell functions instead of external processes.
     -- 
-    -- NB: The functions here that operate on @String@s from @stdin@ read them
-    -- lazily, and can be used in a streaming fashion.
+    -- NB: The functions here that operate on lazy @`ByteString`@s read from
+    -- @stdin@, and can be used in a streaming fashion.
+    -- These reads are lazy. The process is run long enough to produce
+    -- the amount of output that is actually used. It is therefor suitable
+    -- for use with infinite output streams. The feeding process has it's
+    -- @stdout@ closed as soon the function finishes. Note that the result
+    -- is forced to normal form to prevent any accidental reading after
+    -- the process has terminated.
     , pureProc
     , writeOutput, writeError
     , prefixLines
-    , capture
-    , captureTrim
-    , captureSplit
-    , captureSplit0
-    , captureLines
     , readInput
-    , readInputSplit
-    , readInputSplit0
+    , readInputEndBy
+    , readInputEndBy0
     , readInputLines
     , readInputP
-    , readInputSplitP
-    , readInputSplit0P
+    , readInputEndByP
+    , readInputEndBy0P
     , readInputLinesP
     , xargs1
+    -- | === Extracting output to Haskell (Strict)
+    -- These functions are trivially implemented in terms of the above. Note
+    -- that they are strict.
+    , capture
+    , captureTrim
+    , captureEndBy
+    , captureEndBy0
+    , captureLines
     -- | == Piping and Redirection
     , (|>)
     , (|!>)
@@ -46,38 +56,17 @@ module Shh
     , (<|)
     , Stream(..)
     , devNull
-    -- | === Lazy/Streaming reads
-    -- These reads are lazy. The process is run long enough to produce
-    -- the amount of output that is actually used. It is therefor suitable
-    -- for use with infinite output streams. The process is terminated
-    -- as soon the function finishes. Note that the result is forced to
-    -- normal form to prevent any accidental reading after the process has
-    -- terminated.
-    --
-    -- NB: See `readInput` and `pureProc` for more flexible options to those
-    -- listed here.
-    , withRead
-    , withReadSplit0
-    , withReadLines
-    , withReadWords
-    -- | === Strict reads
-    -- NB: See also `capture`
-    , readProc
-    , readTrim
-    , readSplit0
-    , readLines
-    , readWords
-    , readAuto
     -- | === Writing to @stdin@
-    -- NB: See also `writeOutput` for an `echo`-like option.
+    -- NB: See also `writeOutput` for an `echo`-like option. These are all
+    -- implemented in terms of `writeOutput`.
     , (<<<), (>>>), writeProc
-    , readWriteProc
     , apply
     -- | === String manipulation
     -- Utility functions for dealing with common string issues in shell
     -- scripting.
     , trim
-    , split0
+    , endBy
+    , endBy0
     -- | == Exceptions
     -- If any exception is allowed to propagate out of a pipeline, all the
     -- processes comprising the pipeline will be terminated. This is contrary
@@ -87,6 +76,7 @@ module Shh
     , tryFailure
     , exitCode
     -- | == Constructing Arguments
+    , Cmd
     , ExecArg(..)
     , Command()
     , displayCommand
