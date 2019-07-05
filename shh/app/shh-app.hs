4f84392f95de9ce21ae224bec1e04d77830d4153
Luke Clifton <lukec@themk.net>
2019-07-05 14:57:15 +0800

    Fix shh-app

--- shh/app/shh-app.hs
+++ shh/app/shh-app.hs
@@ -83,14 +83,14 @@ main = do
             setOwnerWritable True $
             emptyPermissions
         doIfMissing "init.ghci" $ do
-            catchFailure (exe (pack wrapper) "ghc-pkg" "latest" "shh") >>= \case
+            tryFailure (exe (pack wrapper) "ghc-pkg" "latest" "shh") >>= \case
                 Left _ -> do
                     putStrLn "Please make the shh and shh-extras packages available in the shh"
                     putStrLn "environment (install it globally or modify the wrapper, see docs)."
                     putStrLn "Aborting"
                     exitFailure
                 Right _ -> writeFile "init.ghci" defaultInitGhci
-            catchFailure (exe (pack wrapper) "ghc-pkg" "latest" "shh-extras") >>= \case
+            tryFailure (exe (pack wrapper) "ghc-pkg" "latest" "shh-extras") >>= \case
                 Left _ -> do
                     putStrLn "## WARNING ##########################################################"
                     putStrLn "# You do not have the shh-extras library installed, and so we are"
